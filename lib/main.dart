4eec6adcfed91ee285282b6c9ae95621e368214a
hyeonseo-chae <eric71804122@gmail.com>
2023-10-31 16:03:51 +0900

    시작(start) 페이지랑 언어 선택 페이지 제작함.

--- lib/main.dart
+++ lib/main.dart
@@ -1,186 +1,295 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// 전역 변수로 선택한 언어 저장
+String selectedLanguage = '한국어'; // 기본 언어 설정
+
+class NoTransitionsBuilder extends PageTransitionsBuilder {
+  const NoTransitionsBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+      PageRoute<T> route,
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child,
+      ) {
+    return child; // 애니메이션을 비활성화하고 페이지 전환을 표시하지 않음
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            for (var platform in TargetPlatform.values)
+              platform: NoTransitionsBuilder(),
+          },
+        ),
       ),
+      title: 'Flutter Demo',
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    // 초기화
+    ScreenUtil.init(context);
+
+    return Scaffold(
+      appBar: null,
+      body: Stack(
+        children: [
+          // 반응형 배경 이미지 설정
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/기본-배경.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          // 나머지 UI 요소 추가 가능
+          Center(
+            child: Container(
+              width: 318.w,
+              height: 350.h,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/자산.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          // 영신관 이미지를 버튼으로 사용
+          GestureDetector(
+            onTap: () {
+              // 영신관 이미지를 누를 때 수행할 작업을 정의하세요.
+              print('영신관 이미지를 눌렀습니다.');
+            },
+            child: Align(
+              alignment: Alignment.topCenter, // 상단 중앙에 배치
+              child: Container(
+                width: 350.w,
+                height: 150.h,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/영신관.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // "Start >" 버튼 추가
+          Positioned(
+            left: 84.w,
+            top: 470.h, // 조절하여 원하는 위치로 배치
+            child: GestureDetector(
+              onTap: () {
+                // "Start >" 버튼을 눌렀을 때 다른 화면으로 이동
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SecondPage(), // SecondPage로 이동
+                  ),
+                );
+              },
+              child: Align(
+                alignment: Alignment.topCenter, // 상단 중앙에 배치
+                child: Container(
+                  width: 200.w,
+                  decoration: const BoxDecoration(
+                  color: Colors.transparent, // 배경이 투명
+                  border: null, // 테두리 제거
+                ),
+                  child: Center(
+                    child: Text(
+                      'Start >',
+                      style: TextStyle(
+                        color: Color(0xFF143264), // 글자 색상
+                        fontSize: 35.sp, // 글자 크기
+                        fontFamily: 'YourFontFamily', // 폰트 변경
+                        // 폰트 스타일 변경 (예: fontWeight)
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Container(
-            width: 375,
-            height: 667,
-            clipBehavior: Clip.antiAlias,
-            decoration: BoxDecoration(color: Colors.white),
-            child: Stack(
-              children: [
-                Positioned(
-                  left: 0,
-                  top: 0,
-                  child: Container(
-                    width: 375,
-                    height: 667,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('images/기본-배경.png'),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
+      appBar: null,
+      body: Stack(
+        children: [
+          // 반응형 배경 이미지 설정
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/기본-배경.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          // 나머지 UI 요소 추가 가능
+          Center(
+            child: Container(
+              width: 318.w,
+              height: 350.h,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/자산.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          // 영신관 이미지를 버튼으로 사용
+          GestureDetector(
+            onTap: () {
+              // 영신관 이미지를 누를 때 수행할 작업을 정의하세요.
+              print('영신관 이미지를 눌렀습니다.');
+            },
+            child: Align(
+              alignment: Alignment.topCenter, // 상단 중앙에 배치
+              child: Container(
+                width: 350.w,
+                height: 150.h,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/영신관.png'),
+                    fit: BoxFit.fill,
                   ),
                 ),
-                Positioned(
-                  left: 26,
-                  top: 179,
+              ),
+            ),
+          ),
+          // 언어 선택 버튼 추가
+          Positioned(
+              left: 0.0, // 좌측 정렬
+              right: 0.0, // 우측 정렬
+              top: 470.h, // 조절하여 원하는 위치로 배치
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                GestureDetector(
+                  onTap: () {
+                    // 한국어 선택 버튼을 눌렀을 때
+                    selectedLanguage = '한국어';
+                    print('한국어 선택');
+                  },
                   child: Container(
-                    width: 318,
-                    height: 310,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("images/자산.png"),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 8,
-                  top: 31,
-                  child: Container(
-                    width: 359,
-                    height: 132,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('images/영신관.png'),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 84,
-                  top: 448,
-                  child: Container(
-                    width: 207,
-                    height: 52,
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          left: 0,
-                          top: 5,
-                          child: Container(
-                            width: 207,
-                            height: 42,
-                            decoration: BoxDecoration(color: Color(0x00D9D9D9)),
-                          ),
-                        ),
-                        Positioned(
-                          left: 38,
-                          top: 0,
-                          child: Text(
-                            'Start >',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Color(0xFF143264),
-                              fontSize: 38.13,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.w900,
-                              height: 0.04,
-                            ),
-                          ),
+                    width: 150.w, // 배경의 너비를 텍스트보다 크게 조절
+                    height: 70.h, // 배경의 높이를 텍스트보다 크게 조절
+                    decoration: BoxDecoration(
+                      color: Color(0xFF143264), // 배경 색상을 0xFF143264로 설정
+                      borderRadius: BorderRadius.circular(15.w), // 더 둥글게 만들기
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5), // 그림자 색상
+                          spreadRadius: 2, // 그림자 확산 정도
+                          blurRadius: 5, // 그림자 흐림 정도
+                          offset: Offset(0, 3), // 그림자 위치 (아래로)
                         ),
                       ],
+                    ),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(0.0), // 텍스트 주변에 0.0의 여백 추가
+                        child: Text(
+                          '한국어',
+                          style: TextStyle(
+                            color: Colors.white, // 글자 색상
+                            fontSize: 35.sp, // 글자 크기
+                            fontFamily: 'YourFontFamily', // 폰트 변경
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(width: 20.w), // 버튼 사이 간격
+
+// 영어 선택 버튼
+                GestureDetector(
+                  onTap: () {
+                    // 영어 선택 버튼을 눌렀을 때
+                    selectedLanguage = '영어';
+                    print('영어 선택');
+                  },
+                  child: Container(
+                    width: 150.w, // 배경의 너비를 텍스트보다 크게 조절
+                    height: 70.h, // 배경의 높이를 텍스트보다 크게 조절
+                    decoration: BoxDecoration(
+                      color: Color(0xFF143264), // 배경 색상을 0xFF143264로 설정
+                      borderRadius: BorderRadius.circular(15.w), // 더 둥글게 만들기
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5), // 그림자 색상
+                          spreadRadius: 2, // 그림자 확산 정도
+                          blurRadius: 5, // 그림자 흐림 정도
+                          offset: Offset(0, 3), // 그림자 위치 (아래로)
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(0.0), // 텍스트 주변에 0.0의 여백 추가
+                        child: Text(
+                          'English',
+                          style: TextStyle(
+                            color: Colors.white, // 글자 색상
+                            fontSize: 35.sp, // 글자 크기
+                            fontFamily: 'YourFontFamily', // 폰트 변경
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
-          )
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
