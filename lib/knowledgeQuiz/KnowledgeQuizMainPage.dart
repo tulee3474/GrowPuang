3ef17d81d7c76f3b30fa70b7c139d2290a1c2bc6
hyun-hyang <jimin3060@naver.com>
2023-11-07 02:29:35 +0900

    Feat: knowledgeQuiz folder set

---
+++ lib/knowledgeQuiz/KnowledgeQuizMainPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class KnowledgeQuizMainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: null,
+      body: Container(),
+    );
+  }
+
+}
+
