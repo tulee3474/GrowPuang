16bc7d27467079db8661ca296fe859184be90f02
hyun-hyang <jimin3060@naver.com>
2023-11-07 12:04:24 +0900

    feat: appbar 분리

--- lib/knowledge/firstQuizPage.dart
+++ lib/knowledge/firstQuizPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:growpuang/knowledge/KnowledgeMainPage.dart';
+import 'package:growpuang/view/appBar.dart';
 
 import '../view/home_screen.dart';
 import '../view/navigateBar.dart';
@@ -15,34 +16,7 @@ class firstQuizPage extends StatelessWidget {
       appBar: null,
       body: Stack(
         children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('images/기본-배경.png'),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              print('영신관 이미지를 눌렀습니다.');
-            },
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                width: 350.w,
-                height: 150.h,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('images/영신관.png'),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          appBar(),
           Positioned(
             left: 30.w,
             top: 211.h,
---
+++ lib/view/appBar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:growpuang/knowledge/KnowledgeMainPage.dart';
+
+import '../view/home_screen.dart';
+
+String selectedLanguage = '한국어';
+
+class appBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/기본-배경.png'),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            print('영신관 이미지를 눌렀습니다.');
+          },
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: 350.w,
+              height: 150.h,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/영신관.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
